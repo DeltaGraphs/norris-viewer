208a4b7f9af0788e5eb45da79718335a30e0c4e4
triveking <davide.trive@hotmail.it>
2015-05-12 15:02:06 +0200

    [General] Configurato Robolectric -> DA PROVARE

---
+++ NorrisViewer/app/src/main/java/com/deltagraphs/norrisviewer/MainActivity.java
@@ -0,0 +1,9 @@
+package com.deltagraphs.norrisviewer;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by davide on 12/05/15.
+ */
+public class MainActivity extends ActionBarActivity {
+}
