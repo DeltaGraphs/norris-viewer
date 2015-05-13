719ad225ea7b1950036b1a97d9aae90fdf3d2d8d
blacky8 <blacky8@hotmail.it>
2015-05-13 10:56:23 +0200

    complete PageModelImpl

---
+++ NorrisViewer/app/src/main/java/com/deltagraphs/norrisviewer/model/flowModel/FlowModel.java
@@ -0,0 +1,7 @@
+package com.deltagraphs.norrisviewer.model.flowModel;
+
+/**
+ * Created by Enrico on 13/05/2015.
+ */
+public class FlowModel {
+}
---
+++ NorrisViewer/app/src/main/java/com/deltagraphs/norrisviewer/model/graphsModel/Graph.java
@@ -0,0 +1,7 @@
+package com.deltagraphs.norrisviewer.model.graphsModel;
+
+/**
+ * Created by Enrico on 13/05/2015.
+ */
+public class Graph {
+}
