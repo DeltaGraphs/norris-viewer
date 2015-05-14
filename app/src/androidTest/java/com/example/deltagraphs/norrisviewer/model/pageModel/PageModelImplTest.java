4a41a3086d7ed6a99a391458bf577a979dea1d20
triveking <davide.trive@hotmail.it>
2015-05-14 23:58:23 +0200

    ara qua

--- app/src/androidTest/java/com/example/deltagraphs/norrisviewer/model/pageModel/PageModelImplTest.java
+++ app/src/androidTest/java/com/example/deltagraphs/norrisviewer/model/pageModel/PageModelImplTest.java
@@ -2,9 +2,14 @@ package com.example.deltagraphs.norrisviewer.model.pageModel;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 public class PageModelImplTest extends TestCase {
+
+    private PageModelImpl mPageModelImpl;
 
     @org.junit.Test
     public void testAddPage() throws Exception {
@@ -17,7 +22,13 @@ public class PageModelImplTest extends TestCase {
     }
 
     @org.junit.Test
-    public void testGetList() throws Exception {
-
+    public void AddPagesToList() throws Exception {
+        mPageModelImpl = new PageModelImpl();
+        mPageModelImpl.addPage("Page1","Pagina Uno","questa è la pagina uno");
+        ArrayList<PageModelImpl.Page> list = mPageModelImpl.getList();
+        int dim = list.size();
+        assertEquals("[New page inserted,]", 1, dim);
     }
 }
+
+
