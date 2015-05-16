b5c74ca0431a377967b0bc06dcf86a24fff0f6a8
blacky8 <blacky8@hotmail.it>
2015-05-16 13:02:01 +0200

    move tests

---
+++ app/src/test/java/deltagraphs/norrisviewer/model/pageModel/PageModelImplTest.java
@@ -0,0 +1,45 @@
+package deltagraphs.norrisviewer.model.pageModel;
+
+import junit.framework.TestCase;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class PageModelImplTest extends TestCase {
+
+    private PageModelImpl mPageModelImpl;
+
+    @org.junit.Test
+    public void testAddPage() throws Exception {
+        System.out.println("Testing: testAddPage()");
+        System.out.println("Done!");
+    }
+
+    @org.junit.Test
+    public void testGetName() throws Exception {
+        System.out.println("Testing: testGetName()");
+        System.out.println("Done!");
+    }
+
+    @org.junit.Test
+    public void testAddPagesToList() throws Exception {
+        System.out.println("Testing: AddPagesToList()");
+        mPageModelImpl = new PageModelImpl();
+        mPageModelImpl.addPage("Page1", "Pagina Uno", "questa è la pagina uno");
+        ArrayList<PageModelImpl.Page> list = mPageModelImpl.getList();
+        int dim = list.size();
+        assertEquals("[New page inserted,]", 2, dim);
+        System.out.println("Done!");
+    }
+
+    @org.junit.Test
+    public void testParser() throws Exception {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "Dashboard APS");
+        obj.put("data", "");
+    }
+}
+
+
---
+++ app/src/test/java/deltagraphs/norrisviewer/presenter/SocketManagerTest.java
@@ -0,0 +1,56 @@
+package deltagraphs.norrisviewer.presenter;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ * Name : { Nome del file }.java
+ * Module : com.example.deltagraphs.norrisviewer.presenter
+ * Location : norrisviewer { Percorso in cui \'e presente il file }
+ *
+ * History :
+
+ * Version Date Programmer Description
+ * ===============================================================
+ * 
+ * 0.0.1 2015-05-X davide Creazione file
+ *
+ * ===============================================================
+ *
+ */
+
+public class SocketManagerTest extends TestCase {
+
+    @Test
+    public void testSetSocketUrl() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSocketUrl() throws Exception {
+
+    }
+
+    @Test
+    public void testStartConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testStopConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testCloseSocket() throws Exception {
+
+    }
+
+    @Test
+    public void testStartListening() throws Exception {
+
+    }
+}
