64daab88089bdd6ce6425b465be64c7fc6060347
blacky8 <blacky8@hotmail.it>
2015-05-17 21:34:00 +0200

    fix class

--- app/src/main/java/deltagraphs/norrisviewer/view/mainView/MainActivity.java
+++ app/src/main/java/deltagraphs/norrisviewer/view/mainView/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import deltagraphs.norrisviewer.R;
@@ -47,6 +48,29 @@ public class MainActivity extends ActionBarActivity implements MainView,PageNavi
         super.onCreate(savedInstanceState);
         presenter = new MainPresenterImpl(this);
         showDialog();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
