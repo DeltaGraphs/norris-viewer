83bc6a25f58fd9b4a8be139736cdef330fc8945a
triveking <davide.trive@hotmail.it>
2015-05-23 16:11:34 +0200

    zoom problem

--- app/src/main/java/deltagraphs/norrisviewer/view/graphsView/MapChartActivity.java
+++ app/src/main/java/deltagraphs/norrisviewer/view/graphsView/MapChartActivity.java
@@ -31,28 +31,36 @@ public class MapChartActivity extends FragmentActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(GoogleMap map) {
-        MarkerOptions mOpt = newMarker(map,"801", 10, 10, "shape", "default", "", "", "green");
-        map.addMarker(mOpt);
+        MarkerOptions mOpt = newMarker(map,"823", 10, 10, "icon", "", "bus", "", "green");
+        Marker m = map.addMarker(mOpt);
+        zoomAtPoint(map, m, 400, 500);
     }
 
     public void addMarker(GoogleMap map, MarkerOptions mMarkerOptions){
         map.addMarker(mMarkerOptions);
     }
 
+    public void removeMarker(GoogleMap map, Marker mMarker){
+        if(mMarker != null)
+            mMarker.remove();
+    }
+
+    public void updateMarkerPosition(GoogleMap map, Marker mMarker, float lat, float lng){
+        removeMarker(map, mMarker);
+        mMarker = map.addMarker(new MarkerOptions().position(new LatLng(lat, lng)));
+    }
+
     public MarkerOptions newMarker(GoogleMap map, String id, float lat, float lng,String type, String shape, String icon, String text, String color){
 
         MarkerOptions m = new MarkerOptions();
-
+        m.position(new LatLng(lat, lng));
+        m.title(id);
         switch (type) {
             // shape marker
             case "shape":
                 switch(shape) {
-
-
                     //normal marker
                     default:
-                        m.position(new LatLng(lat, lng));
-                        m.title(id);
                         switch (color) {
                             case "red":
                                 m.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
@@ -88,29 +96,26 @@ public class MapChartActivity extends FragmentActivity implements OnMapReadyCall
                 }
 
             case "icon":
-                m.position(new LatLng(lat, lng));
-                m.title(id);
                 switch (icon){
-                    /*
                     case "bus":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.bus_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.bus_marker));
                         break;
                     case "flag":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.flag_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.flag_marker));
                         break;
                     case "car":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.car_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.car_marker));
                         break;
                     case "house":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.house_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.house_marker));
                         break;
                     case "man":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.man_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.man_marker));
                         break;
                     case "woman":
-                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.woman_marker)));
+                        m.icon(BitmapDescriptorFactory.fromResource(R.mipmap.woman_marker));
                         break;
-                    */
+
                 }
             case "text":
             default:
@@ -132,6 +137,39 @@ public class MapChartActivity extends FragmentActivity implements OnMapReadyCall
             default:
                 map.setMapType(GoogleMap.MAP_TYPE_NONE);
         }
+    }
+
+
+    public void zoomAtPoint(GoogleMap map, Marker mMarker, int width, int height){
+        LatLng position = mMarker.getPosition();
+        double[] ref = getBoundingBox(position.latitude, position.longitude, width, height);
+        LatLngBounds bounds = new LatLngBounds(new LatLng(ref[0], ref[1]), new LatLng( ref[2], ref[3]));
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
+        map.animateCamera(CameraUpdateFactory.zoomIn());
+    }
+
+    private double[] getBoundingBox(final double pLatitude, final double pLongitude, final int widthInMeters, final int heightInMeters) {
+
+        final double[] boundingBox = new double[4];
+
+        final double latRadian = Math.toRadians(pLatitude);
+
+        final double degLatKm = 110.574235;
+        final double degLongKm = 110.572833 * Math.cos(latRadian);
+        final double deltaLat = widthInMeters / 1000.0 / degLatKm;
+        final double deltaLong = heightInMeters / 1000.0 / degLongKm;
+
+        final double minLat = pLatitude - deltaLat;
+        final double minLong = pLongitude - deltaLong;
+        final double maxLat = pLatitude + deltaLat;
+        final double maxLong = pLongitude + deltaLong;
+
+        boundingBox[0] = minLat;
+        boundingBox[1] = minLong;
+        boundingBox[2] = maxLat;
+        boundingBox[3] = maxLong;
+
+        return boundingBox;
     }
 
 
