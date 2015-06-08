16c41c6f3fb6d7e3fa2cf89d11ac57379f8f026c
triveking <davide.trive@hotmail.it>
2015-06-08 16:37:35 +0200

    go balcky

--- app/src/main/java/deltagraphs/norrisviewer/presenter/mainPresenter/MainPresenterImpl.java
+++ app/src/main/java/deltagraphs/norrisviewer/presenter/mainPresenter/MainPresenterImpl.java
@@ -70,7 +70,7 @@ public class MainPresenterImpl implements MainPresenter, Observer {
         // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         //input.setText("http://norris-nrti-dev.herokuapp.com/page1/map1");
-        input.setText("http://apsupervisor-dev.herokuapp.com/norris");
+        input.setText("http://norris-nrti-dev.herokuapp.com/norris");
         builder.setView(input);
 
         // Set up the buttons
